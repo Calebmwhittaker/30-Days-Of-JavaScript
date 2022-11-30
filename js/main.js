af911d8d28d16f1ec3916f16192df66264313368
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2022-11-30 08:02:20 -0500

    Day 7 Lesson Part 2.1

--- js/main.js
+++ js/main.js
@@ -84,9 +84,82 @@
     parameter */
 
 //Function with Unlimited Number of Parameters
-function sumAllNums() {
-  console.log(arguments);
-  /*The arguments variable is required here */
-}
-sumAllNums(1, 2, 3);
+// function sumAllNums() {
+//   console.log(arguments);
+//   /*The arguments variable is required here */
+// }
+// sumAllNums(1, 2, 3);
 //[Arguments] {'0': 1, '1': 2, '2' : 3}
+
+// function sumAllNums() {
+//   let sum = 0;
+//   for (let i = 0; i < arguments.length; i++) {
+//     sum += arguments[i];
+//   }
+//   return sum;
+// }
+// console.log(sumAllNums(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+//UNLIMITED NUMBER OF PARAMETERS IN ARROW FUNCTION
+// const sumAllNums = (...args) => {
+//   let sum = 0;
+//   for (const element of args) {
+//     sum += element;
+//   }
+//   return sum;
+// };
+// console.log(sumAllNums(1, 2, 3, 4));
+/*The spread operator (...) in the ...args makes it an array that can be iterated through */
+
+//ANONYMOUS FUNCTION
+// const anonymousFunc = function () {
+//   console.log("I am an anonymous function");
+// };
+// console.log(anonymousFunc());
+
+//EXPRESSION FUNCTION
+// const square = function (n) {
+//   return n * n;
+// };
+// console.log(square(2));
+
+//SELF-INVOKING FUNCTIONS
+// (function (n) {
+//   console.log(n * n);
+// })(70);
+
+// let squaredNums = (function (n) {
+//   return n * n;
+// })(2);
+// console.log(squaredNums);
+
+//ARROW FUNCTION
+// const square = (n) => {
+//   return n * n;
+// };
+// console.log(square(2));
+// const square = (n) => n * n; //Single-lined functions do not require a return keyword
+// console.log(square(5));
+
+// const changeToUpperCase = (arr) => {
+//   const newArr = [];
+//   for (const element of arr) {
+//     newArr.push(element.toUpperCase());
+//   }
+//   return newArr; //DONT FORGET THE RETURN STATEMENT
+// };
+// const countries = ["Finland", "Sweden", "Norway", "Denmark", "Iceland"];
+// console.log(changeToUpperCase(countries));
+
+// const printFullName = (firstName, lastName) => `${firstName} ${lastName}`;
+
+// console.log(printFullName("Caleb", "Whittaker")); //Returns undefined if you omit an argument from that parameter
+
+//FUNCTION WITH DEFAULT PARAMETERS
+// function greetings(name = "Peter") {
+//   //You can give a default parameter to your function to prevent undefined from being returned in the console
+//   let message = `Welcome ${name}!`;
+//   return message;
+// }
+// console.log(greetings());
+// console.log(greetings("Caleb"));
