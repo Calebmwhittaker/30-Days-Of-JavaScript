01ab0fc43815f0f5af67a353e21f0b93bb078867
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2023-01-04 21:49:28 -0500

    Day 20 Lesson Completed

---
+++ main.js
@@ -0,0 +1,27 @@
+//DAY 20 LESSON
+//JavaScript Style Guide
+/*A JavaScript style guid is a set of standards that tells how JavaScript code should be written and organized */
+
+//Why we need Style Guides
+/*Common JavaScript Style Guides:
+    Airbnb JSC
+    JavaScript Standard SC
+    Google JavaScript Style Guide */
+
+//Airbnb JavaScript Style Guide
+/*One of the most popular JavaScript style guides on the internet */
+
+//Common Conventions for Writing Clean Code
+/*Conventions:
+    camelCase for variables and functions
+    ALL variable names start with a letter
+    Use of 'const' for constants, arrays, objects, or functions
+    Use of single quotes or backticks
+    Semicolons are not necessary and are of personal preference
+    Includes spaces around arithmetic operators, assignments and after commas
+    Use of arrow functions instead of function declarations
+    Use of explicit returns instead of implicit returns if a function is a one-liner
+    No trailing comma in the last value of an object
+    Use of the +=, -=, *=, /= **= instead of having to redeclare the variable
+    Print a tag string when using console.log() to know where the console is coming from
+    Array names are plural */
