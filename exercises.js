d6bb367f7d19187085b1adc4bb8bcac3cbbb9b1e
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2023-01-09 18:08:20 -0500

    Day 23 Exercises Part 2

--- exercises.js
+++ exercises.js
@@ -1,14 +1,14 @@
 //DAY 23 EXERCISES
 //LEVEL 1
 //1.
+document.body.style.textAlign = "center";
+
 const h1 = document.createElement("h1");
 const h2 = document.createElement("h2");
 const h3 = document.createElement("h3");
 
 const input = document.createElement("input");
 const button = document.createElement("button");
-
-document.body.style.textAlign = "center";
 
 document.body.appendChild(h1);
 h1.textContent = "Number Generator";
@@ -21,29 +21,55 @@ document.body.appendChild(input);
 input.setAttribute("type", "text");
 input.setAttribute("id", "number");
 input.setAttribute("placeholder", "Type in a number");
+input.addEventListener("focus", removeNewRow);
 document.body.appendChild(button);
 button.textContent = "Generate Numbers";
+button.style.backgroundColor = "green";
+button.style.color = "white";
+button.addEventListener("click", clickEventHandler);
 
-const row = document.createElement("div");
-row.style.margin = "20px auto";
+const newRow = document.createElement("div");
 
-button.addEventListener(
-  "click",
-  () => {
-    const value = input.value;
-    const pattern = /[0-9]/;
-    if (value.match(pattern)) {
-      for (let i = 1; i <= value; i++) {
-        const numberParagraph = document.createElement("p");
-        const col = document.createElement("div");
-        col.setAttribute("class", "col-2");
-        col.appendChild(numberParagraph);
-        numberParagraph.textContent = i;
-        row.appendChild(col);
-      }
-      row.setAttribute("class", "row");
-      document.body.appendChild(row);
+const isPrime = (x) => {
+  for (let i = 2; i <= 100; i++) {
+    for (let j = 2; j <= i; j++) {
+      if (i * j == x) return false;
     }
-  },
-  { once: true }
-);
+  }
+  return true;
+};
+
+function clickEventHandler() {
+  newRow.style.margin = "20px auto";
+  newRow.style.justifyContent = "space-around";
+  const value = input.value;
+  const pattern = /[0-9]/;
+  if (value.match(pattern)) {
+    for (let i = 1; i <= value; i++) {
+      const numberParagraph = document.createElement("p");
+      const col = document.createElement("div");
+      col.setAttribute("class", "col-2 m-1");
+      col.style.alignContent = "center";
+      col.appendChild(numberParagraph);
+      numberParagraph.style.marginBottom = "0";
+      if (i % 2 === 0) {
+        col.style.backgroundColor = "red";
+      } else {
+        col.style.backgroundColor = "lightblue";
+      }
+      if (isPrime(i) && i !== 1) {
+        col.style.backgroundColor = "yellow";
+      }
+      numberParagraph.textContent = i;
+      newRow.appendChild(col);
+    }
+    newRow.setAttribute("class", "row");
+    document.body.appendChild(newRow);
+    button.removeEventListener("click", clickEventHandler);
+  }
+}
+
+function removeNewRow() {
+  newRow.remove();
+  button.addEventListener("click", clickEventHandler);
+}
