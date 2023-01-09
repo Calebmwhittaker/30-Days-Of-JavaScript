c6903cce2663051bfd48def9662d0396f835b988
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2023-01-09 08:26:10 -0500

    Day 23 Lesson Completed

---
+++ js/Day22/main.js
@@ -0,0 +1,55 @@
+//DAY 22 LESSON
+//Creating an Element
+/*createElement('tagname'): A document method that allows you to create a new HTML element by taking a tagname as a string parameter */
+// document.createElement("tagname");
+// let title = document.createElement("h1");
+// title.className = "title";
+// title.style.fontSize = "24px";
+// title.textContent = "Creating HTML element DOM Day 2";
+// console.log(title);
+
+//Creating Elements
+/**\To create multiple elements, we should use a loop */
+// let title;
+// for (let i = 0; i < 3; i++) {
+//   title = document.createElement("h1");
+//   title.className = "title";
+//   title.style.fontSize = "24px";
+//   title.textContent = i;
+//   console.log(title);
+// }
+
+//Appending Child to a Parent Element
+/*To see a created element on the document, we should append it to the parent as a child element */
+/*appendChild(): A document method that allows you to append a child element to a parent element */
+// let title;
+// for (let i = 0; i < 3; i++) {
+//   title = document.createElement("h1");
+//   title.className = "title";
+//   title.style.fontSize = "24px";
+//   title.textContent = i;
+//   document.body.appendChild(title);
+// }
+/*Body is the parent element when using the appendChild method */
+
+//Remove a Child Element from a Parent Node
+/*After creating an HTML element, you may want to remove the element */
+/*removeChild(): A document method that allows you to remove a child element from a parent element */
+// const ul = document.querySelector("ul");
+// document.body.appendChild(ul);
+// const lists = document.querySelectorAll("li");
+// document.appendChild(lists);
+// for (const list of lists) {
+//   ul.removeChild(lists);
+// }
+// const ul = document.querySelector("ul");
+// document.body.appendChild(ul);
+// const li = document.querySelectorAll("li");
+// document.ul.appendChild(li);
+// for (const list of li) {
+//   console.log(list);
+//   ul.removeChild(list);
+// }
+
+// const ul = document.querySelector("ul");
+// ul.textContent = "";
--- main.js
+++ main.js
@@ -1,55 +1,107 @@
-//DAY 22 LESSON
-//Creating an Element
-/*createElement('tagname'): A document method that allows you to create a new HTML element by taking a tagname as a string parameter */
-// document.createElement("tagname");
-// let title = document.createElement("h1");
-// title.className = "title";
-// title.style.fontSize = "24px";
-// title.textContent = "Creating HTML element DOM Day 2";
-// console.log(title);
+//DAY 23 LESSON
+//Event Listeners
+/*Common HTML events:
+    onclick
+    onchange
+    onmouseover
+    onkeydown
+    onkeyuponload */
+/*Use 'addEventListener() method to 'listen' to different types of HTML events on HTML elements */
+/*addEventListener() takes two parameters: "eventListener" and callback function */
+// selectedElement.addEventListener("eventListener", function (e) {
+//   //Activity you want to occur after the even will be in here
+// });
 
-//Creating Elements
-/**\To create multiple elements, we should use a loop */
-// let title;
-// for (let i = 0; i < 3; i++) {
-//   title = document.createElement("h1");
-//   title.className = "title";
-//   title.style.fontSize = "24px";
-//   title.textContent = i;
-//   console.log(title);
-// }
+// //OR
 
-//Appending Child to a Parent Element
-/*To see a created element on the document, we should append it to the parent as a child element */
-/*appendChild(): A document method that allows you to append a child element to a parent element */
-// let title;
-// for (let i = 0; i < 3; i++) {
-//   title = document.createElement("h1");
-//   title.className = "title";
-//   title.style.fontSize = "24px";
-//   title.textContent = i;
-//   document.body.appendChild(title);
-// }
-/*Body is the parent element when using the appendChild method */
+// selectedElement.addEventListener("eventListener", (e) => {
+//   //Activity you want to occur after the even will be in here
+// });
 
-//Remove a Child Element from a Parent Node
-/*After creating an HTML element, you may want to remove the element */
-/*removeChild(): A document method that allows you to remove a child element from a parent element */
-// const ul = document.querySelector("ul");
-// document.body.appendChild(ul);
-// const lists = document.querySelectorAll("li");
-// document.appendChild(lists);
-// for (const list of lists) {
-//   ul.removeChild(lists);
-// }
-// const ul = document.querySelector("ul");
-// document.body.appendChild(ul);
-// const li = document.querySelectorAll("li");
-// document.ul.appendChild(li);
-// for (const list of li) {
-//   console.log(list);
-//   ul.removeChild(list);
-// }
+//Click
+/*To attach an event listener to an element, we first have to select the element we want to attach the addEventListener method to. */
+/*The addEventListener() method takes an event type and callback function as an argument */
+// const button = document.querySelector("button");
+// button.addEventListener("click", (e) => {
+//   console.log("e gives the event listener object: ", e);
+//   console.log("e.target give the selected element: ", e.target);
+//   console.log(
+//     "e.target.textContent gives content of selected element: ",
+//     e.target.textContent
+//   );
+// });
 
-// const ul = document.querySelector("ul");
-// ul.textContent = "";
+//An event can also be attached directly to the HTML element as inline scripts
+
+//Double Click
+// const button = document.querySelector("button");
+// button.addEventListener("dblclick", (e) => {
+//   console.log("e gives the event listener object:", e);
+//   console.log("e.target gives the selected element: ", e.target);
+//   console.log(
+//     "e.target.textContent gives content of selected element: ",
+//     e.target.textContent
+//   );
+// });
+
+//Mouse Enter
+// const button = document.querySelector("button");
+// button.addEventListener("mouseenter", (e) => {
+//   console.log("e gives the event listener object:", e);
+//   console.log("e.target gives the selected element: ", e.target);
+//   console.log(
+//     "e.target.textContent gives content of selected element: ",
+//     e.target.textContent
+//   );
+// });
+
+//Types of JavaScript Events
+/*click - when the element clicked
+dblclick - when the element double clicked
+mouseenter - when the mouse point enter to the element
+mouseleave - when the mouse pointer leave the element
+mousemove - when the mouse pointer move on the element
+mouseover - when the mouse pointer move on the element
+mouseout -when the mouse pointer out from the element
+input -when value enter to input field
+change -when value change on input field
+blur -when the element is not focused
+keydown - when a key is down
+keyup - when a key is up
+keypress - when we press any key
+onload - when the browser has finished loading a page*/
+
+//Getting values from an input element
+// const mass = document.querySelector("#mass");
+// const height = document.querySelector("#height");
+// const button = document.querySelector("button");
+
+// let bmi;
+// button.addEventListener("click", (e) => {
+//   bmi = mass.value / height.value ** 2;
+//   alert(`Your BMI is ${bmi.toFixed(1)}`);
+//   console.log(bmi.toFixed(2));
+// });
+
+//Input Event and Change
+// const input = document.querySelector("input");
+// const p = document.querySelector("p");
+
+// input.addEventListener("input", (e) => {
+//   p.textContent = e.target.value; //e.target is the input in this case
+// });
+
+//Blur Event
+/*The blur event occurs when the input field is not in focus */
+// const input = document.querySelector("input");
+// const p = document.querySelector("p");
+
+// input.addEventListener("blur", (e) => {
+//   p.textContent = "Field is required";
+//   p.style.color = "red";
+// });
+
+//keypress, keydown, keyup
+document.body.addEventListener("keypress", (e) => {
+  alert(e.location);
+});
