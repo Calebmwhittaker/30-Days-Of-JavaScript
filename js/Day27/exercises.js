1e4ab2fb97c97c13e9c025b0da505e950a517204
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2023-01-11 18:17:07 -0500

    Day 27 Part 2

--- js/Day27/exercises.js
+++ js/Day27/exercises.js
@@ -2,21 +2,22 @@ import { asabenehChallenges2020 } from "../../modules/challenge.js";
 let { keywords, challenges, author } = asabenehChallenges2020;
 let { titles } = author;
 const div = document.querySelector("#titles-div");
-const titleParagraph = document.createElement("p");
-div.appendChild(titleParagraph);
-titleParagraph.textContent = titles[0];
-for (const title of titles) {
-  const p = document.createElement("p");
-  const joinedTitle = title.join("");
-  p.textContent = joinedTitle;
-  div.appendChild(p);
-}
+// for (const title of titles) {
+//   const p = document.createElement("p");
+//   const joinedTitle = title.join("");
+//   p.textContent = joinedTitle;
+//   div.appendChild(p);
+// }
 const materialsSection = document.querySelector("#materials");
 for (let i = 0; i < 3; i++) {
   let { name } = challenges[i];
   const div = document.createElement("div");
   div.textContent = name;
   div.setAttribute("class", "card");
+  div.style.height = "180px";
+  div.style.width = "180px";
+  div.style.alignItems = "center";
+  div.style.justifyContent = "center";
   materialsSection.appendChild(div);
 }
 const teachingSection = document.querySelector("#teaching");
@@ -25,3 +26,13 @@ for (const keyword of keywords) {
   teachingDiv.textContent = `The technologies I teach ${keyword}`;
   teachingSection.appendChild(teachingDiv);
 }
+// setInterval(() => {
+//   for (const title of titles) {
+//     setInterval(() => {
+//       const p = document.createElement("p");
+//       const joinedTitle = title.join("");
+//       p.textContent = joinedTitle;
+//       div.appendChild(p);
+//     }, 3000);
+//   }
+// }, 3000);
