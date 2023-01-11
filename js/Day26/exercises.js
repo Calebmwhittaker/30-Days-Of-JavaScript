9d16d87099abd9c4cbee86eeab7668fdf04006a3
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2023-01-10 19:20:56 -0500

    Day26 Exercises Part 1

---
+++ js/Day26/exercises.js
@@ -0,0 +1,9 @@
+//DAY 26 EXERCISES
+//LEVEL 1
+//1.
+document.body.style.textAlign = "center";
+const header = document.createElement("header");
+header.style.backgroundImage = 'url("../images/globe.jpeg")';
+const h1 = document.createElement("h1");
+h1.textContent = "WORLD COUNTRIES LIST";
+document.body.appendChild(header);
