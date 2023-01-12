056ef3df6939455f68c1d4595b273fa3c7549bf7
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2023-01-11 21:29:42 -0500

    Day 27 Exercises Part 3

--- js/Day27/exercises.js
+++ js/Day27/exercises.js
@@ -1,13 +1,42 @@
 import { asabenehChallenges2020 } from "../../modules/challenge.js";
+function randomColor() {
+  let colorArray = [];
+  for (let i = 0; i < 6; i++) {
+    let choice = Math.floor(Math.random() * 2);
+    if (choice === 0) {
+      const letters = "abcdef";
+      const letter = letters[Math.floor(Math.random() * letters.length)];
+      colorArray.push(letter);
+    } else {
+      let number = Math.floor(Math.random() * 10);
+      colorArray.push(number);
+    }
+  }
+  const joinedArray = colorArray.join("");
+  return `#${joinedArray}`;
+}
+console.log(randomColor());
 let { keywords, challenges, author } = asabenehChallenges2020;
 let { titles } = author;
+const newTitles = titles.map((title) => title.join(" "));
 const div = document.querySelector("#titles-div");
-// for (const title of titles) {
-//   const p = document.createElement("p");
-//   const joinedTitle = title.join("");
-//   p.textContent = joinedTitle;
-//   div.appendChild(p);
-// }
+let skillsCounter = 0;
+let wordCounter = 0;
+let wordLength = keywords.length - 1;
+let titlesLength = newTitles.length - 1;
+div.textContent = newTitles[0];
+
+function skills() {
+  if (skillsCounter === titlesLength) {
+    skillsCounter = 0;
+    div.textContent = newTitles[skillsCounter];
+  } else {
+    skillsCounter++;
+    div.textContent = newTitles[skillsCounter];
+  }
+}
+setInterval(skills, 2500);
+
 const materialsSection = document.querySelector("#materials");
 for (let i = 0; i < 3; i++) {
   let { name } = challenges[i];
@@ -21,18 +50,25 @@ for (let i = 0; i < 3; i++) {
   materialsSection.appendChild(div);
 }
 const teachingSection = document.querySelector("#teaching");
-const teachingDiv = document.createElement("div");
-for (const keyword of keywords) {
-  teachingDiv.textContent = `The technologies I teach ${keyword}`;
-  teachingSection.appendChild(teachingDiv);
+const teachingParagraph = document.createElement("p");
+teachingParagraph.style.display = "inline";
+const keywordParagraph = document.createElement("p");
+keywordParagraph.setAttribute("id", "keyword");
+keywordParagraph.textContent = `${keywords[wordCounter]}`;
+keywordParagraph.style.display = "inline";
+keywordParagraph.style.fontSize = "40px";
+teachingParagraph.textContent = `The technologies I teach `;
+teachingParagraph.appendChild(keywordParagraph);
+keywordParagraph.style.color = `${randomColor()}`;
+
+function keywordsFunc() {
+  if (wordCounter === wordLength) {
+    wordCounter = 0;
+  } else {
+    wordCounter++;
+    keywordParagraph.textContent = `${keywords[wordCounter]}`;
+    keywordParagraph.style.color = `${randomColor()}`;
+  }
 }
-// setInterval(() => {
-//   for (const title of titles) {
-//     setInterval(() => {
-//       const p = document.createElement("p");
-//       const joinedTitle = title.join("");
-//       p.textContent = joinedTitle;
-//       div.appendChild(p);
-//     }, 3000);
-//   }
-// }, 3000);
+setInterval(keywordsFunc, 2500);
+teachingSection.appendChild(teachingParagraph);
