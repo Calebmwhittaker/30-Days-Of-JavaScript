777a7f90a0f60e2bedbc3090b895a7e13acc026d
Calebmwhittaker <100395705+Calebmwhittaker@users.noreply.github.com>
2023-01-16 15:39:54 -0500

    Day 30 Exercises Part 3

--- js/Day30/exercises.js
+++ js/Day30/exercises.js
@@ -2,36 +2,13 @@ const countriesTemplate = document.querySelector(".countries-card-template");
 const graphTemplate = document.querySelector(".graph-template");
 const searchBar = document.querySelector("input");
 const nameButton = document.querySelector(".name-button");
-const capitlaButton = document.querySelector(".capital-button");
+const capitalButton = document.querySelector(".capital-button");
 const populationButton = document.querySelector(".population-button");
+const graphImg = document.querySelector(".graph-img");
+const countriesSection = document.body.querySelector(".countries-section");
 
 let countriesArray = [];
-
-nameButton.addEventListener("click", () => {
-  searchBar.addEventListener("input", () => {
-    const value = searchBar.value.toLowerCase();
-    countriesArray.forEach((country) => {
-      const isVisible = country.name.toLowerCase().match(value);
-      country.element.classList.toggle("hide", !isVisible);
-    });
-  });
-});
-
-capitlaButton.addEventListener("click", () => {
-  searchBar.addEventListener("input", () => {
-    const value = searchBar.value.toLowerCase();
-    countriesArray.forEach((country) => {
-      if (country.capital) {
-        console.table(country.capital[0]);
-        const isVisible = country.capital[0].match(value);
-        if (isVisible) {
-          console.log(isVisible);
-        }
-        country.element.classList.toggle("hide", !isVisible);
-      }
-    });
-  });
-});
+let order = false;
 
 try {
   fetch("https://restcountries.com/v3.1/all")
@@ -55,49 +32,132 @@ try {
         })
         .sort((a, b) => (a.name > b.name ? 1 : -1));
 
-      countriesArray = countriesArray.map((country) => {
-        const countriesSection =
-          document.body.querySelector(".countries-section");
-        const cloneCountries =
-          countriesTemplate.content.cloneNode(true).children[0];
-        const countryFlagDiv = cloneCountries.querySelector(".flag-div");
-        const countryFlagImg = countryFlagDiv.querySelector(".flag-img");
-        const countryName = cloneCountries.querySelector(".name");
-        const countryCapital = cloneCountries.querySelector(".capital");
-        const countryLanguages = cloneCountries.querySelector(".languages");
-        const countryPopulation = cloneCountries.querySelector(".population");
-        function getLanguagesOfEachCountry() {
-          let languagesArray = [];
-          for (const language in country.languages) {
-            const newLanguage = country.languages[language];
-            languagesArray.push(newLanguage);
-          }
-          const joined = languagesArray.join(", ");
-          return joined;
-        }
-        countryFlagImg.setAttribute("src", country.flags);
-        countryFlagImg.setAttribute("alt", `Flag of the ${country.name}`);
-        countryName.textContent = country.name;
-        if (country.capital) {
-          countryCapital.textContent = `Capital: ${country.capital[0]}`;
-        } else {
-          countryCapital.textContent = `Capital: No Capital`;
-        }
-        countryLanguages.textContent = `Languages: ${getLanguagesOfEachCountry()}`;
-        countryPopulation.textContent = `Population: ${Intl.NumberFormat(
-          "en-US",
-          { type: "decimal" }
-        ).format(country.population)}`;
+      function createCountryTemplate() {
+        countriesArray = countriesArray.map((country) => {
+          const cloneCountries =
+            countriesTemplate.content.cloneNode(true).children[0];
+          const countryFlagDiv = cloneCountries.querySelector(".flag-div");
+          const countryFlagImg = countryFlagDiv.querySelector(".flag-img");
+          const countryName = cloneCountries.querySelector(".name");
+          const countryCapital = cloneCountries.querySelector(".capital");
+          const countryLanguages = cloneCountries.querySelector(".languages");
+          const countryPopulation = cloneCountries.querySelector(".population");
 
-        countriesSection.append(cloneCountries);
-        return {
-          name: country.name,
-          languages: country.languages,
-          capital: country.capital,
-          population: country.population,
-          element: cloneCountries,
-        };
+          function getLanguagesOfEachCountry() {
+            let languagesArray = [];
+            for (const language in country.languages) {
+              const newLanguage = country.languages[language];
+              languagesArray.push(newLanguage);
+            }
+            const joined = languagesArray.join(", ");
+            return joined;
+          }
+
+          countryFlagImg.setAttribute("src", country.flags);
+          countryFlagImg.setAttribute("alt", `Flag of the ${country.name}`);
+          countryName.textContent = country.name;
+          if (country.capital !== undefined) {
+            countryCapital.textContent = `Capital: ${country.capital[0]}`;
+            country.capital = country.capital;
+          } else {
+            countryCapital.textContent = `Capital: No Capital`;
+            country.capital = "No Capital";
+          }
+          countryLanguages.textContent = `Languages: ${getLanguagesOfEachCountry()}`;
+          countryPopulation.textContent = `Population: ${Intl.NumberFormat(
+            "en-US",
+            { type: "decimal" }
+          ).format(country.population)}`;
+
+          countriesSection.appendChild(cloneCountries);
+
+          return {
+            name: country.name,
+            languages: country.languages,
+            capital: country.capital,
+            population: country.population,
+            flags: country.flags,
+            element: cloneCountries,
+          };
+        });
+      }
+
+      nameButton.addEventListener("click", () => {
+        order = !order;
+        countriesArray.sort((a, b) => {
+          if (order) {
+            if (a.name > b.name) {
+              return -1;
+            } else {
+              return 1;
+            }
+          } else {
+            if (a.name > b.name) {
+              return 1;
+            } else {
+              return -1;
+            }
+          }
+        });
+        for (let i = 0; i < countriesArray.length; i++) {
+          countriesSection.removeChild(countriesSection.firstChild);
+        }
+        createCountryTemplate();
+        searchBar.addEventListener("input", () => {
+          const value = searchBar.value.toLowerCase();
+          countriesArray.forEach((country) => {
+            const isVisible = country.name.toLowerCase().match(value);
+            country.element.classList.toggle("hide", !isVisible);
+          });
+        });
       });
+
+      capitalButton.addEventListener("click", () => {
+        order = !order;
+        countriesArray.sort((a, b) => {
+          if (order) {
+            if (a.capital > b.capital) {
+              return -1;
+            } else {
+              return 1;
+            }
+          } else {
+            if (a.capital > b.capital) {
+              return 1;
+            } else {
+              return -1;
+            }
+          }
+        });
+        for (let i = 0; i < countriesArray.length; i++) {
+          countriesSection.removeChild(countriesSection.firstChild);
+        }
+        createCountryTemplate();
+        searchBar.addEventListener("input", () => {
+          const value = searchBar.value.toLowerCase();
+          countriesArray.forEach((country) => {
+            const stringCapital = country.capital.toString();
+            const isVisible = stringCapital.toLowerCase().match(value);
+            country.element.classList.toggle("hide", !isVisible);
+          });
+        });
+      });
+
+      populationButton.addEventListener("click", () => {
+        order = !order;
+        countriesArray.sort((a, b) =>
+          order ? b.population - a.population : a.population - b.population
+        );
+        for (let i = 0; i < countriesArray.length; i++) {
+          countriesSection.removeChild(countriesSection.firstChild);
+        }
+        createCountryTemplate();
+      });
+
+      graphImg.addEventListener("click", () => {
+        console.log("hello");
+      });
+      createCountryTemplate();
     });
 } catch (err) {
   console.log(err);
